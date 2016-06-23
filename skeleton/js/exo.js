73c564fd77990fb22124b1732cda2de5bec2d3a4
David Maser <davidemaser@gmail.com>
2016-06-23 16:34:32 -0400

    Initial Core Build

---
+++ skeleton/js/exo.js
@@ -0,0 +1,19 @@
+/**
+ * Created by david-maser on 23/06/16.
+ */
+var exo = {
+    init:function(){
+        $(this).version = 0.1;
+    },
+    load:function(){
+        if (typeof exo === "function") {
+            //start all here
+        }
+    },
+    log:function(e){
+        console.log(e);
+    },
+    animate:function(obj,type,speed,length){
+
+    }
+};
