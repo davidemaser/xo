05eaa584862d870face61b7c30b510c681a02ff4
David Maser <davidemaser@gmail.com>
2016-06-30 10:56:50 -0400

    Add require css and modulate init

--- skeleton/examples/scripts/init.js
+++ skeleton/examples/scripts/init.js
@@ -1,14 +1,28 @@
 /**
  * Created by david-maser on 29/06/16.
  * require.js app init
+ * uses require.css to load css with
+ * the same init model
  */
 requirejs.config({
     baseUrl: '../lib/',
+    map: {
+        '*': {
+            'css': 'bower_components/require-css/css' // path to require-css
+        }
+    },
+    shim : {
+        "bootstrap" : { "deps" :['jquery'] },
+        "xo" : { "deps" :['jquery'] }
+    },
     paths: {
-        jquery: 'bower_components/jquery/dist/jquery',
-        bootstrap: 'bower_components/bootstrap/dist/js/bootstrap.min'
+        "jquery" : 'bower_components/jquery/dist/jquery',
+        "bootstrap" :  'bower_components/bootstrap/dist/js/bootstrap.min'
     }
 });
-define(['jquery', 'bootstrap'], function (jquery) {
-    var bootLoad = require('bootstrap');//load bootstrap once jq is loaded
+
+require(['jquery', 'bootstrap','css!../dist/css/xo.min'], function($){
+    $(function(){
+        //all good let's code
+    });
 });
