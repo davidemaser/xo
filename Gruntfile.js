ccf7ecabdf970743dd8cd9639cf31cecec7c3f92
david Maser <david.maser@altitude-sports.com>
2016-07-22 13:04:32 -0400

    Added grunt plugins

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,21 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        devUpdate: {
+            main: {
+                options: {
+                    updateType: 'report', //just report outdated packages 
+                    reportUpdated: false, //don't report up-to-date packages 
+                    semver: true, //stay within semver when updating 
+                    packages: {
+                        devDependencies: true, //only check for devDependencies 
+                        dependencies: false
+                    },
+                    packageJson: null, //use matchdep default findup to locate package.json 
+                    reportOnlyPkgs: [] //use updateType action on all packages 
+                }
+            }
+        },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -22,13 +37,48 @@ module.exports = function(grunt) {
                     'skeleton/dist/css/<%= pkg.name %>.min.css': 'skeleton/sass/<%= pkg.name %>.scss'
                 }
             }
+        },
+        htmlmin: {                                     // Task
+            dist: {                                      // Target
+                options: {                                 // Target options
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {                                   // Dictionary of files
+                    'skeleton/examples/dist/index.html': 'skeleton/examples/index.html'
+                }
+            },
+            dev: {                                       // Another target
+                files: {
+                    'skeleton/examples/dist/index.html': 'skeleton/examples/index.html'
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: '**/*.js',
+                tasks: ['jshint','uglify'],
+                options: {
+                    debounceDelay: 250
+                }
+            },
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass'],
+                options: {
+                    livereload: true
+                }
+            }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-dev-update');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
     // Default task(s).
-    grunt.registerTask('default', ['uglify','sass']);
+    grunt.registerTask('default', ['devUpdate','uglify','sass','htmlmin']);
 
 };
